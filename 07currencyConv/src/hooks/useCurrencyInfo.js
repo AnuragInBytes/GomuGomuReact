000553a897a9ab0584217b8e6d763bae24724c87
Anurag <anuragpoddar9484@gmail.com>
2024-04-01 20:23:24 +0530

    added unfinished currency exchanger

---
+++ 07currencyConv/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+
+function useCurrencyInfo(currency){
+
+    let url = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-31/v1/currencies/${currency}.json`
+
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        fetch(url)
+            .then((res) => res.json())
+            .then((res) => setData(res[currency]))
+    }, [currency])
+    return data;
+}
+
+export default useCurrencyInfo;
